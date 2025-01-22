8be66c78d9537bf56045b75f1495974a5b9fa9b6
pdevulapally <128475072+pdevulapally@users.noreply.github.com>
2025-01-22 00:39:25 +0000

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,19 +1,18 @@
-
 // Firebase imports
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-app.js";
 import { getFirestore, collection, addDoc, serverTimestamp } 
 from "https://www.gstatic.com/firebasejs/11.0.2/firebase-firestore.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-analytics.js";
 
-// Firebase configuration loaded from environment variables
+// Firebase configuration
 const firebaseConfig = {
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.FIREBASE_APP_ID,
-    measurementId: process.env.FIREBASE_MEASUREMENT_ID
+    apiKey: "AIzaSyDr6xcvSmCXqH2XD-PxZeuBF3dXkdJRdHM",
+    authDomain: "my-portfolio-b204e.firebaseapp.com",
+    projectId: "my-portfolio-b204e",
+    storageBucket: "my-portfolio-b204e.firebasestorage.app",
+    messagingSenderId: "609036685786",
+    appId: "1:609036685786:web:9b809a3d7e47d05d9730e3",
+    measurementId: "G-V0CB9N0QLB"
 };
 
 const app = initializeApp(firebaseConfig);
